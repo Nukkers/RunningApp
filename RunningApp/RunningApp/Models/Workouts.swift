54ec54350fb2caa17a5f008eda2be63fbcdd9b4d
Naukhez Ali <naukhez.3@hotmail.co.uk>
2021-02-17 10:08:16 +0000

    Updated workout model to no longer depend on LocationManager.

--- RunningApp/RunningApp/Models/Workouts.swift
+++ RunningApp/RunningApp/Models/Workouts.swift
@@ -6,54 +6,31 @@
 //
 
 import Foundation
-import Combine
 
 class Workout: Identifiable, Codable {
-    var startTime: Date
     var distance: Measurement<UnitLength>
+    var startTime: Date
+    var locationCoord: [WorkoutLocation] = []
+    var location: WorkoutLocation?
+    var placemark: String?
     
-    init(distance: Measurement<UnitLength>, startTime: Date) {
+    init(distance: Measurement<UnitLength>, startTime: Date, locationCoords: [WorkoutLocation], placemark: String,
+         location: WorkoutLocation) {
         self.distance = distance
         self.startTime = startTime
+        self.locationCoord = locationCoords
+        self.placemark = placemark
+        self.location = location
+        
     }
 }
 
-class WorkoutManager: ObservableObject {
-    //    var locationCoord: [CLLocation]
-    //    var placemark: String
-    //    var location: CLLocation
-    //    var status: CLAuthorizationStatus
-    //    @Published var distance: Measurement<UnitLength>
-    //    var startTime: Date
-    var workouts: [Workout]
-    static let saveKey = "SavedData"
+struct WorkoutLocation: Codable {
+    var latitude: Double
+    var longitude: Double
     
-    init() {
-        self.workouts = []
-        self.workouts = load()
-         
-    }
-    
-    func load() -> [Workout] {
-        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
-            if let decoded = try? JSONDecoder().decode([Workout].self, from: data) {
-                self.workouts = decoded
-                return self.workouts
-            }
-        }
-        return []
-    }
-    
-    func add(_ newWorkout: Workout) {
-        self.workouts.append(newWorkout)
-        save()
-        print("New workout added succesfully: \(newWorkout.distance) + \(newWorkout.startTime)")
-    }
-    
-    private func save() {
-        if let encoded = try? JSONEncoder().encode(self.workouts) {
-            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
-            print("Workouts saved")
-        }
+    init(lat: Double, long: Double) {
+        self.latitude = lat
+        self.longitude = long
     }
 }
