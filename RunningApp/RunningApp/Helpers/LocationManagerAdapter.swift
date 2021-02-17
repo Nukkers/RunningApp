a05dec4fcdb8dcab226238b2cd361cd404925acb
Naukhez Ali <naukhez.3@hotmail.co.uk>
2021-02-17 10:10:13 +0000

    Created an adapter to convert data between locationManager and my workout model

---
+++ RunningApp/RunningApp/Helpers/LocationManagerAdapter.swift
@@ -0,0 +1,34 @@
+//
+//  LocationManagerAdapter.swift
+//  RunningApp
+//
+//  Created by Naukhez Ali on 17/02/2021.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManagerAdapter {
+    
+    func convertPlacemark(place: CLPlacemark) -> String  {
+        return place.description
+    }
+    
+    func convertCLLocationToWorkoutLocation(cllocations: [CLLocation]) -> [WorkoutLocation] {
+        
+        var workoutLocation = [WorkoutLocation]()
+        
+        for cllocation in cllocations {
+            let workout = WorkoutLocation(lat: cllocation.coordinate.latitude, long: cllocation.coordinate.longitude)
+            workoutLocation.append(workout)
+        }
+        
+        return workoutLocation
+    }
+    
+    func convertLocationToWorkoutLocation(location: CLLocation) -> WorkoutLocation {
+        let workoutLocation = WorkoutLocation(lat: location.latitude, long: location.longitude)
+        
+        return workoutLocation
+    }
+}
